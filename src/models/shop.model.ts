9e3528622689fb3a4416b9c21fff5e522aa8a6b5
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-11-25 18:35:59 +0800

    Add card product properties

--- src/models/shop.model.ts
+++ src/models/shop.model.ts
@@ -2,7 +2,10 @@ import { AuditTrail, Card } from 'models';
 
 export type CardProduct = AuditTrail & {
   id: string;
+  name: string;
   price: number;
-  card: Card;
+  cards: Card[];
+  description?: string;
+  image?: any;
   slug?: string;
 };
