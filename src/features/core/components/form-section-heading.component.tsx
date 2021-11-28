7e781ad13b94dab5acfdfa69df6f6509169f6e63
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-11-28 21:21:27 +0800

    Add right component

--- src/features/core/components/form-section-heading.component.tsx
+++ src/features/core/components/form-section-heading.component.tsx
@@ -1,11 +1,29 @@
-import { FC } from 'react';
-import { Box, Heading, Divider, BoxProps } from '@chakra-ui/react';
+import { FC, ReactNode } from 'react';
+import { Box, Flex, FlexProps, Heading, Divider, BoxProps } from '@chakra-ui/react';
 
-export const FormSectionHeading: FC<BoxProps> = ({ children, ...moreProps }) => (
+type Props = BoxProps & {
+  rightComponent?: ReactNode;
+  flexProps?: FlexProps;
+};
+
+export const FormSectionHeading: FC<Props> = ({
+  rightComponent,
+  flexProps,
+  children,
+  ...moreProps
+}) => (
   <Box py={4} {...moreProps}>
-    <Heading fontSize='2xl' as='h6'>
-      {children}
-    </Heading>
+    <Flex justifyContent='space-between' alignItems='center' {...flexProps}>
+      <Heading flex={1} fontSize='2xl' as='h6'>
+        {children}
+      </Heading>
+      {rightComponent}
+    </Flex>
     <Divider />
   </Box>
 );
+
+FormSectionHeading.defaultProps = {
+  rightComponent: undefined,
+  flexProps: undefined
+};
