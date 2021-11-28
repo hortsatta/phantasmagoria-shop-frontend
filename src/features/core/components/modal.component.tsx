7f67fda45fd9ec43dae5b8a3d4ff11505c799004
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-11-28 21:21:04 +0800

    Add modal component to core

---
+++ src/features/core/components/modal.component.tsx
@@ -0,0 +1,28 @@
+import { FC } from 'react';
+import {
+  Modal as ChakraModal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalContentProps,
+  ModalOverlay,
+  ModalProps
+} from '@chakra-ui/react';
+
+type Props = ModalProps & {
+  modalContentProps?: ModalContentProps;
+};
+
+export const Modal: FC<Props> = ({ modalContentProps, children, ...moreProps }) => (
+  <ChakraModal isCentered {...moreProps}>
+    <ModalOverlay />
+    <ModalContent bg='rgba(0,0,0,0)' {...modalContentProps}>
+      <ModalCloseButton _focus={{ boxShadow: 0 }} />
+      <ModalBody p={0}>{children}</ModalBody>
+    </ModalContent>
+  </ChakraModal>
+);
+
+Modal.defaultProps = {
+  modalContentProps: undefined
+};
