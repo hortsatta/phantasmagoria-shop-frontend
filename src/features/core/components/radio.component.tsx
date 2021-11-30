cf2af5e74925a601dea604a58d03e2cf28860ce1
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-11-30 20:11:54 +0800

    Add radio and search core components

---
+++ src/features/core/components/radio.component.tsx
@@ -0,0 +1,33 @@
+import { FC } from 'react';
+import { Box, useRadio } from '@chakra-ui/react';
+import { Circle, CheckCircle } from 'phosphor-react';
+
+import variables from 'assets/styles/_variables.module.scss';
+
+export const Radio: FC = ({ children, ...moreProps }) => {
+  const { getInputProps, getCheckboxProps } = useRadio(moreProps);
+  const input: any = getInputProps();
+  const checkbox = getCheckboxProps();
+
+  return (
+    <Box w='100%' as='label'>
+      <input {...input} />
+      <Box
+        {...checkbox}
+        pl='8px'
+        pr='12px'
+        py='3px'
+        d='flex'
+        alignItems='center'
+        bgColor='none'
+        borderRadius='2px'
+        boxShadow='md'
+        cursor='pointer'
+        _checked={{ bgColor: variables.accentColor }}
+      >
+        <Box mr={2}>{input.checked ? <CheckCircle weight='bold' /> : <Circle weight='bold' />}</Box>
+        {children}
+      </Box>
+    </Box>
+  );
+};
---
+++ src/features/core/components/search-input.component.tsx
@@ -0,0 +1,29 @@
+import { FC, ReactNode } from 'react';
+import {
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputProps,
+  InputRightElement
+} from '@chakra-ui/react';
+import { Planet } from 'phosphor-react';
+
+import { Icon } from './icon.component';
+
+type Props = InputProps & {
+  rightComponent?: ReactNode;
+};
+
+export const SearchInput: FC<Props> = ({ rightComponent, ...moreProps }) => (
+  <InputGroup borderRadius={999} overflow='hidden'>
+    <InputLeftElement px={3} height='100%' boxSizing='content-box' pointerEvents='none'>
+      <Icon as={Planet} w={6} opacity={0.7} />
+    </InputLeftElement>
+    <Input pl={14} h='44px' placeholder='Find an entity...' {...moreProps} />
+    {rightComponent && <InputRightElement>{rightComponent}</InputRightElement>}
+  </InputGroup>
+);
+
+SearchInput.defaultProps = {
+  rightComponent: undefined
+};
