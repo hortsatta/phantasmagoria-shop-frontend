17900096a3cf67693f5ed33529b108919feee9e5
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-11-30 23:20:15 +0800

    Add debounce loading

--- src/features/core/hooks/use-debounce-value.hook.ts
+++ src/features/core/hooks/use-debounce-value.hook.ts
@@ -1,6 +1,8 @@
 import { useEffect, useState } from 'react';
 import { DEBOUNCE_DURATION } from './use-debounce.hook';
 
+const DEBOUNCE_LOADING_DURATION = 500;
+
 export const useDebounceValue = (
   value: any,
   delay = DEBOUNCE_DURATION
@@ -12,14 +14,18 @@ export const useDebounceValue = (
     setLoading(true);
     // Update debounced value after delay
     const debounce = setTimeout(() => {
-      setLoading(false);
       setDebouncedValue(value);
     }, delay);
+    // Further delay loading to give time to execute desired function
+    const debounceLoading = setTimeout(() => {
+      setLoading(false);
+    }, delay + DEBOUNCE_LOADING_DURATION);
     // Cancel the timeout if value changes (also on delay change or unmount)
     // This is how we prevent debounced value from updating if value is changed ...
     // .. within the delay period. Timeout gets cleared and restarted.
     return () => {
       clearTimeout(debounce);
+      clearTimeout(debounceLoading);
     };
   }, [value, delay]);
 
