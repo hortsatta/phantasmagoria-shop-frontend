71cd9c768f2896ffcb99261fafaaf4c9b84f7b33
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-11-30 23:21:06 +0800

    Implement framer animation

--- src/features/shop/components/shop-item.component.tsx
+++ src/features/shop/components/shop-item.component.tsx
@@ -1,9 +1,9 @@
-import { FC, useMemo } from 'react';
+import { ComponentProps, FC, useMemo } from 'react';
 import { Button, Divider, Flex, Heading, Image } from '@chakra-ui/react';
 import { Brain, Knife, Tray } from 'phosphor-react';
 
 import { CardProduct } from 'models';
-import { Icon, IconButton, Surface } from 'features/core/components';
+import { Icon, IconButton, Skeleton, MotionSurface } from 'features/core/components';
 
 import variables from 'assets/styles/_variables.module.scss';
 
@@ -11,24 +11,61 @@ type Props = {
   item: CardProduct;
 };
 
-export const ShopItem: FC<Props> = ({ item }) => {
-  const { id, name, image, cards } = item;
+const itemAnim = {
+  hidden: { opacity: 0, transform: 'translateY(32px)' },
+  show: { opacity: 1, transform: 'translateY(0px)' }
+};
+
+const wrapperProps: ComponentProps<typeof MotionSurface> = {
+  flexDir: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+  m: 4,
+  w: '272px',
+  borderColor: 'rgba(255,255,255,0.06)',
+  borderWidth: '1px',
+  borderRadius: 8,
+  overflow: 'hidden',
+  layout: true
+};
+
+const ShopItemSkeleton: FC = () => (
+  <MotionSurface
+    {...wrapperProps}
+    initial={{ opacity: 0, transform: 'translateY(32px)' }}
+    animate={{ opacity: 1, transform: 'translateY(0px)' }}
+  >
+    <Skeleton w='100%' h='212px' />
+    <Flex w='100%' flexDir='column' alignItems='center'>
+      <Divider />
+      <Flex justifyContent='center' alignItems='center' w='100%' h='40px'>
+        <Skeleton w='70%' h={5} />
+      </Flex>
+      <Divider w='90%' />
+      <Flex pos='relative' justifyContent='center' alignItems='flex-start' w='100%' h='64px'>
+        <Flex flex={1} h='100%' justifyContent='center' alignItems='center'>
+          <Skeleton w='32px' h='32px' />
+        </Flex>
+        <Divider h='90%' orientation='vertical' />
+        <Flex flex={1} h='100%' justifyContent='center' alignItems='center'>
+          <Skeleton w='32px' h='32px' />
+        </Flex>
+        <Divider h='90%' orientation='vertical' />
+        <Flex flex={1} h='100%' justifyContent='center' alignItems='center'>
+          <Skeleton w='32px' h='32px' />
+        </Flex>
+      </Flex>
+    </Flex>
+  </MotionSurface>
+);
+
+const ShopItem: FC<Props> = ({ item }) => {
+  const { name, image, cards } = item;
   const itemImage = useMemo(() => image?.url || cards[0].coverImage.url, [item]);
 
   return (
-    <Surface
-      key={id}
-      flexDir='column'
-      justifyContent='center'
-      alignItems='center'
-      m={4}
-      w='272px'
-      borderColor='rgba(255,255,255,0.06)'
-      borderWidth='1px'
-      borderRadius={8}
-      overflow='hidden'
-    >
-      <Image src={itemImage} bgColor='rgba(0,0,0,0.3)' />
+    <MotionSurface {...wrapperProps} variants={itemAnim}>
+      <Image src={itemImage} h='212px' bgColor='rgba(0,0,0,0.3)' objectFit='cover' />
       <Flex w='100%' flexDir='column' alignItems='center'>
         <Divider />
         <Button
@@ -78,6 +115,8 @@ export const ShopItem: FC<Props> = ({ item }) => {
           />
         </Flex>
       </Flex>
-    </Surface>
+    </MotionSurface>
   );
 };
+
+export { ShopItem, ShopItemSkeleton };
--- src/features/shop/components/shop-list.component.tsx
+++ src/features/shop/components/shop-list.component.tsx
@@ -1,24 +1,75 @@
-import { FC } from 'react';
-import { Flex } from '@chakra-ui/react';
+import { FC, ReactNode } from 'react';
+import { Flex, Text } from '@chakra-ui/react';
 
 import { CardProduct } from 'models';
-import { ShopItem } from './shop-item.component';
+import { MotionSurface } from 'features/core/components';
+import { ShopItem, ShopItemSkeleton } from './shop-item.component';
+
+import variables from 'assets/styles/_variables.module.scss';
 
 type Props = {
   items: CardProduct[];
   loading?: boolean;
 };
 
+type PlaceholderProps = {
+  children?: ReactNode;
+  loading?: boolean;
+};
+
+const wrapperAnim = {
+  hidden: { opacity: 0 },
+  show: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.05
+    }
+  }
+};
+
+const Placeholder: FC<PlaceholderProps> = ({ loading, children }) => (
+  // eslint-disable-next-line react/no-array-index-key
+  <>{loading ? [1, 2, 3].map(key => <ShopItemSkeleton key={key} />) : children}</>
+);
+
 export const ShopList: FC<Props> = ({ items, loading }) => (
-  <Flex flexDir='column'>
-    <Flex flex={1} alignItems='flex-start' justifyContent='center' flexWrap='wrap'>
-      {items.map((item: CardProduct) => (
-        <ShopItem key={item.id} item={item} />
-      ))}
+  <Flex flexDir='column' w='100%'>
+    <Flex flex={1} alignItems='flex-start' justifyContent='flex-start' flexWrap='wrap' w='100%'>
+      <Placeholder loading={loading}>
+        <MotionSurface
+          w='100%'
+          bgColor='transparent'
+          borderColor='transparent'
+          borderWidth={0}
+          borderRadius={8}
+          variants={wrapperAnim}
+          initial='hidden'
+          animate='show'
+        >
+          {items.length ? (
+            items.map((item: CardProduct) => <ShopItem key={item.id} item={item} />)
+          ) : (
+            <Text
+              mt={8}
+              w='100%'
+              textAlign='center'
+              fontFamily={variables.primaryFont}
+              fontSize='4xl'
+            >
+              we could only show nothing.
+            </Text>
+          )}
+        </MotionSurface>
+      </Placeholder>
     </Flex>
   </Flex>
 );
 
 ShopList.defaultProps = {
+  loading: false
+};
+
+Placeholder.defaultProps = {
+  children: undefined,
   loading: false
 };
