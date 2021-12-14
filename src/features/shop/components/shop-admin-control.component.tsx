7e94969fdcc3c01b5ff42111dcece4ecc02ee338
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-12-14 10:00:43 +0800

    Add section header and list button

--- src/features/shop/components/shop-admin-control.component.tsx
+++ src/features/shop/components/shop-admin-control.component.tsx
@@ -1,42 +1,78 @@
 import { FC, useMemo } from 'react';
 import { useHistory } from 'react-router-dom';
 import { useReactiveVar } from '@apollo/client';
-import { Button, VStack } from '@chakra-ui/react';
+import { Box, Button, ButtonGroup, ButtonProps, Text, TextProps, VStack } from '@chakra-ui/react';
 import { PlusCircle, StackSimple } from 'phosphor-react';
 
 import { appModulesVar } from 'config';
 import { Icon } from 'features/core/components';
 
+const sectionHeaderStyles: TextProps = {
+  mb: 2,
+  color: 'rgba(255,255,255,0.7)',
+  fontSize: 13,
+  textTransform: 'uppercase'
+};
+
+const buttonStyles: ButtonProps = {
+  variant: 'ghost',
+  size: 'sm'
+};
+
 export const ShopAdminControl: FC = () => {
   const history = useHistory();
   const appModules: any = useReactiveVar(appModulesVar);
+
   const addCardPath = useMemo(
-    () => `${appModules.card.path}${appModules.card.children?.add.path}`,
+    () => `${appModules.admin.path}${appModules.card.path}${appModules.card.children?.add.path}`,
     [appModules]
   );
+
   const addShopItemPath = useMemo(
-    () => `${appModules.shop.path}${appModules.shop.children?.add.path}`,
+    () => `${appModules.admin.path}${appModules.shop.path}${appModules.shop.children?.add.path}`,
     [appModules]
   );
 
   return (
-    <VStack flexDir='column' alignItems='flex-start' spacing={4}>
-      <Button
-        variant='ghost'
-        size='sm'
-        leftIcon={<Icon w={7} as={PlusCircle} />}
-        onClick={() => history.push(addShopItemPath)}
-      >
-        Add Item
-      </Button>
-      <Button
-        variant='ghost'
-        size='sm'
-        leftIcon={<Icon w={7} as={StackSimple} />}
-        onClick={() => history.push(addCardPath)}
-      >
-        Add New Card
-      </Button>
+    <VStack flexDir='column' alignItems='flex-start'>
+      <Box>
+        <Text {...sectionHeaderStyles}>Shop Item</Text>
+        <ButtonGroup isAttached>
+          <Button
+            {...buttonStyles}
+            leftIcon={<Icon w={7} as={PlusCircle} />}
+            onClick={() => history.push(addShopItemPath)}
+          >
+            Add New
+          </Button>
+          <Button
+            {...buttonStyles}
+            leftIcon={<Icon w={7} as={StackSimple} />}
+            onClick={() => history.push(addCardPath)}
+          >
+            View Items
+          </Button>
+        </ButtonGroup>
+      </Box>
+      <Box>
+        <Text {...sectionHeaderStyles}>Card</Text>
+        <ButtonGroup isAttached>
+          <Button
+            {...buttonStyles}
+            leftIcon={<Icon w={7} as={PlusCircle} />}
+            onClick={() => history.push(addCardPath)}
+          >
+            Add New
+          </Button>
+          <Button
+            {...buttonStyles}
+            leftIcon={<Icon w={7} as={StackSimple} />}
+            onClick={() => history.push(addCardPath)}
+          >
+            View Cards
+          </Button>
+        </ButtonGroup>
+      </Box>
     </VStack>
   );
 };
