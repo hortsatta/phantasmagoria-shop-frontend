ec25c4b74f87a14138125d74115b08a369bd5217
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-12-24 22:43:00 +0800

    Move loading overlay

--- src/features/shop/components/shop-list.component.tsx
+++ src/features/shop/components/shop-list.component.tsx
@@ -1,7 +1,8 @@
-import { FC, ReactNode } from 'react';
-import { Center, Flex, Spinner, Text } from '@chakra-ui/react';
+import { FC, useMemo } from 'react';
+import { Flex, Text } from '@chakra-ui/react';
 
 import { CardProduct } from 'models';
+import { LoadingOverlay } from 'features/core/components';
 import { ShopItem } from './shop-item.component';
 
 import variables from 'assets/styles/_variables.module.scss';
@@ -16,22 +17,6 @@ type Props = {
   onEditClick?: (item: CardProduct) => void;
 };
 
-type LoadingOverlayProps = {
-  children?: ReactNode;
-  loading?: boolean;
-};
-
-const LoadingOverlay: FC<LoadingOverlayProps> = ({ loading, children }) => (
-  <>
-    {loading && (
-      <Center pos='absolute' top={0} left={0} w='100%' h='352px' bgColor='transparent' zIndex={1}>
-        <Spinner size='xl' />
-      </Center>
-    )}
-    {children}
-  </>
-);
-
 export const ShopList: FC<Props> = ({
   items,
   loading,
@@ -40,44 +25,48 @@ export const ShopList: FC<Props> = ({
   onDetailClick,
   onFavoriteClick,
   onEditClick
-}) => (
-  <Flex pos='relative' flexDir='column' w='100%'>
-    <LoadingOverlay loading={loading}>
-      <Flex
-        flex={1}
-        alignItems='flex-start'
-        justifyContent='flex-start'
-        flexWrap='wrap'
-        w='100%'
-        opacity={loading ? 0.5 : 1}
-        transition='opacity 0.2s ease'
-      >
-        {items.map((item: CardProduct) => (
-          <ShopItem
-            key={item.id}
-            item={item}
-            loading={processingItems[item.id]}
-            {...(onCartClick && { onCartClick })}
-            {...(onDetailClick && { onDetailClick })}
-            {...(onFavoriteClick && { onFavoriteClick })}
-            {...(onEditClick && { onEditClick })}
-          />
-        ))}
-        {!items.length && !loading && (
-          <Text
-            mt={8}
-            w='100%'
-            textAlign='center'
-            fontFamily={variables.primaryFont}
-            fontSize='4xl'
-          >
-            we could only show nothing.
-          </Text>
-        )}
-      </Flex>
-    </LoadingOverlay>
-  </Flex>
-);
+}) => {
+  const isEmpty = useMemo(() => !items.length && !loading, [items, loading]);
+
+  return (
+    <Flex pos='relative' flexDir='column' w='100%'>
+      <LoadingOverlay loading={loading}>
+        <Flex
+          flex={1}
+          alignItems='flex-start'
+          justifyContent='flex-start'
+          flexWrap='wrap'
+          w='100%'
+          opacity={loading ? 0.5 : 1}
+          transition='opacity 0.2s ease'
+        >
+          {items.map((item: CardProduct) => (
+            <ShopItem
+              key={item.id}
+              item={item}
+              loading={processingItems[item.id]}
+              {...(onCartClick && { onCartClick })}
+              {...(onDetailClick && { onDetailClick })}
+              {...(onFavoriteClick && { onFavoriteClick })}
+              {...(onEditClick && { onEditClick })}
+            />
+          ))}
+          {isEmpty && (
+            <Text
+              mt={8}
+              w='100%'
+              textAlign='center'
+              fontFamily={variables.primaryFont}
+              fontSize='4xl'
+            >
+              we could only show nothing.
+            </Text>
+          )}
+        </Flex>
+      </LoadingOverlay>
+    </Flex>
+  );
+};
 
 ShopList.defaultProps = {
   loading: false,
@@ -86,9 +75,4 @@ ShopList.defaultProps = {
   onDetailClick: undefined,
   onFavoriteClick: undefined,
   onEditClick: undefined
-};
-
-LoadingOverlay.defaultProps = {
-  children: undefined,
-  loading: false
 };
