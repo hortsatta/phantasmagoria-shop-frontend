1e9b95ad119586de5dc2dd837ed76584bebd83b1
Erwin Errol Calanuga <hortsatta@gmail.com>
2021-12-27 08:36:39 +0800

    Add Cart model

--- src/models/cart.model.ts
+++ src/models/cart.model.ts
@@ -1,8 +1,15 @@
 import { AuditTrail } from './core.model';
 import { CardProduct } from './shop.model';
 
-export type CartItem = AuditTrail & {
+type CartItem = AuditTrail & {
   id: string;
   quantity: number;
   cardProduct: CardProduct;
 };
+
+type Cart = AuditTrail & {
+  id: string;
+  cartItems: CartItem[];
+};
+
+export type { Cart, CartItem };
