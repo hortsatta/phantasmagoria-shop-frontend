fd96a60af9bc70a12ea90dcbb598e5762d94c9ac
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-01-12 14:10:20 +0800

    Add icon property

--- src/features/core/components/remove-icon-button.component.tsx
+++ src/features/core/components/remove-icon-button.component.tsx
@@ -1,14 +1,15 @@
-import { FC, useState } from 'react';
+import { FC, ReactElement, useState } from 'react';
 import { WarningCircle, XCircle } from 'phosphor-react';
 
 import { IconButton, Icon } from 'features/core/components';
 
 type Props = {
   onClick: () => void;
+  icon?: ReactElement;
   disabled?: boolean;
 };
 
-export const RemoveIconButton: FC<Props> = ({ onClick, disabled }) => {
+export const RemoveIconButton: FC<Props> = ({ onClick, icon, disabled }) => {
   const [confirmRemove, setConfirmRemove] = useState(false);
 
   const handleRemove = () => {
@@ -28,7 +29,7 @@ export const RemoveIconButton: FC<Props> = ({ onClick, disabled }) => {
   ) : (
     <IconButton
       aria-label='remove'
-      icon={<Icon as={XCircle} w={7} />}
+      icon={icon || <Icon as={XCircle} w={7} />}
       onClick={() => setConfirmRemove(true)}
       disabled={disabled}
     />
@@ -36,5 +37,6 @@ export const RemoveIconButton: FC<Props> = ({ onClick, disabled }) => {
 };
 
 RemoveIconButton.defaultProps = {
+  icon: undefined,
   disabled: false
 };
