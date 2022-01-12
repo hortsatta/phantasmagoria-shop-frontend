bd42e3f4f01de4708f2873ffeb4316bc75f032f7
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-01-12 14:08:13 +0800

    Implement click outside hook

--- src/features/core/hooks/index.ts
+++ src/features/core/hooks/index.ts
@@ -1,4 +1,5 @@
 export * from './use-debounce.hook';
 export * from './use-debounce-value.hook';
 export * from './use-notification.hook';
+export * from './use-on-click-outside.hook';
 export * from './use-url-query.hook';
---
+++ src/features/core/hooks/use-on-click-outside.hook.ts
@@ -0,0 +1,22 @@
+/* eslint-disable no-undef */
+import { useEffect } from 'react';
+
+export const useOnClickOutside = (ref: any, callback: any) => {
+  useEffect(() => {
+    const listener = (event: any) => {
+      // Do nothing if clicking ref's element or descendent elements
+      if (!ref.current || ref.current.contains(event.target)) {
+        return;
+      }
+      callback(event);
+    };
+
+    document.addEventListener('mousedown', listener);
+    document.addEventListener('touchstart', listener);
+
+    return () => {
+      document.removeEventListener('mousedown', listener);
+      document.removeEventListener('touchstart', listener);
+    };
+  }, [ref, callback]);
+};
