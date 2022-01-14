dbd28a954465ed2c159fb25b08b5f295e1f7f409
Erwin Errol Calanuga <hortsatta@gmail.com>
2022-01-14 12:38:37 +0800

    Add orders detail query

--- src/services/graphql/order.graphql.ts
+++ src/services/graphql/order.graphql.ts
@@ -51,6 +51,67 @@ const GET_ORDERS = gql`
   }
 `;
 
+const GET_ORDERS_DETAIL = gql`
+  query GetOrders(
+    $where: JSON
+    $limit: Int
+    $start: Int
+    $sort: String = "date:desc"
+    $publicationState: PublicationState = LIVE
+  ) {
+    orders(
+      where: $where
+      limit: $limit
+      start: $start
+      sort: $sort
+      publicationState: $publicationState
+    ) {
+      id
+      date
+      totalPrice
+      address {
+        id
+        fullName
+        phoneNumber
+        region
+        province
+        city
+        barangay
+        zipCode
+        addressLine
+      }
+      orderItems {
+        price
+        quantity
+        cardProduct: card_product {
+          id
+          name
+          slug
+          image {
+            id
+            name
+            url
+          }
+          cards {
+            id
+            name
+            image {
+              id
+              name
+              url
+            }
+            coverImage {
+              id
+              name
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`;
+
 const CREATE_ORDER = gql`
   mutation CreateOrder($order: OrderInput) {
     createOrder(input: { data: $order }) {
@@ -71,4 +132,4 @@ const CREATE_ORDER = gql`
   }
 `;
 
-export { UPSERT_PAYMENT_INTENT, GET_PAYMENT_INTENT, GET_ORDERS, CREATE_ORDER };
+export { UPSERT_PAYMENT_INTENT, GET_PAYMENT_INTENT, GET_ORDERS, GET_ORDERS_DETAIL, CREATE_ORDER };
